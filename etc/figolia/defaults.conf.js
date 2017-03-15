176e8e3e35404b167db57f7eccdf60ea0f84abe8
David Young <dave@artichokelabs.com>
2017-03-15 16:31:22 -0700

    config file for figolia, super basic - needs some support from us in terms of timestamps

---
+++ etc/figolia/defaults.conf.js
@@ -0,0 +1,101 @@
+/*eslint-disable */
+//
+// algolia-firebase-indexer main config file
+// -----------------------------------------
+//
+// You must modify this file before running the server
+//
+// Mandatory information is :
+//   * You Algolia and Firebase keys
+//   * The schema/indexes you want to sync
+//
+
+var CONFIG = {
+    // Firebase credentials
+    firebase: {
+        instance: process.env.FIREBASE_INSTANCE || 'whatsgood-f1e9b',
+        secret: process.env.FIREBASE_SECRET || 'prECtY7aFMzZdoZbYSif2Bbr4bvGDS2FjX8x6xLK',
+        // Where to store server metadata
+        path: process.env.FIREBASE_PATH || 'algolia',
+        // Firebase token will be generated with this uid (to write above path)
+        uid: process.env.FIREBASE_UID || 'algolia'
+    },
+    // Algolia credentials
+    algolia: {
+        applicationId: process.env.ALGOLIA_APP_ID || '2OEMW8KEZS',
+        // *Admin* API Key
+        apiKey: process.env.ALGOLIA_API_KEY || 'eda259be0c749313ac33e0d07e465443'
+    },
+    // Fully reindex all datasets. Can be overiden on the commandline.
+    reset: false,
+    // Stay running and live-index all firebase operations.
+    // Can be overriden on the commandline.
+    liveIndex: false,
+    // Minimum throttle delay between Algolia API calls (in seconds)
+    // Note: between each throttle delay, a maximum of
+    // [3 * dataset number] calls can be made (add, update & delete)
+    throttleDelay: 10,
+    // Optional but strongly reccomended, this field will be checked against last
+    // run date to see if reindexing is necessary.
+    // Field content must be UNIX timestamp (example Date.now() output).
+    // WARNING: Without this field being correctly configured,
+    // everything is re-indexed at rerun.
+    // timestampField: 'updatedAt',
+    // Firebase datasets to index in Algolia
+    schema: {
+        subjects: {
+            // Firebase path
+            path: 'subjects',
+            // Algolia index (must exist already)
+            index: 'whatsgood-subjects',
+            // Optional, name of ID field (otherwise,
+            // the Firebase object key will be used)
+            // key: 'id',
+            // Optional, dataset-specific update time field
+            // (default is use global setting above)
+            // timestampField: 'createdAt',
+            // Optional, list of fields to index
+            // (otherwise, every field will be indexed)
+            includeFields: [
+                'title',
+                'description'
+            ],
+            // Optional, list of fields to exclude from index
+            // Note: if both are specified, `excludeFields`
+            // is applied *after* `includeFields`
+            // excludeFields: [
+            //     'passwdHash',
+            //     'private'
+            // ],
+            // Optional, list of fields for which you need N-Gram tokens
+            // ex. "username: 'hermione'", will also create an additional
+            // field "usernameNGrams: ['ermione', 'rmione', 'mione', 'ione', 'one']"
+            // for each word (down to 3 chars), making *infix* search possible
+            // in addition to default prefix search available in Algolia out of the box
+            // letting users search with keyword "mione"
+            // Note: this can be storage-consumming for long fields, use with
+            //       caution ! (preferably on fields with enforced size)
+            // ngrams: ['username']
+        },
+        reviewsBySubject: {
+            // Second example dataset to index
+            path: 'reviews-by-subject',
+            index: 'whatsgood-reviews'
+        }
+    }
+};
+
+//
+// Uncomment this for production setup.
+//
+
+// if (process.env.NODE_ENV === 'production') {
+//
+//     CONFIG.firebase.instance = 'TO_BE_CHANGED';
+//     CONFIG.firebase.secret = 'TO_BE_CHANGED';
+//     CONFIG.schema.todoLists.index = 'prod_todo_lists';
+//     CONFIG.schema.todoItems.index = 'prod_todo_items';
+//
+// }
+
+module.exports = CONFIG;
