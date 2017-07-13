ef0c6cfcd95e98a73be3bbe60a4920ee87682c70
brian <leung.b@gmail.com>
2017-07-12 19:11:10 -0700

    switch to browserHistory, get rid of # in url

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/App';
 import Review from './components/Review';
@@ -30,7 +30,7 @@ injectTapEventPlugin();
 
 ReactDOM.render((
   <Provider store={store}>
-    <Router history={hashHistory}>
+    <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
         <Route path="global" component={GlobalFeed} />
