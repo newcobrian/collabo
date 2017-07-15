685f7a1bb301e666375225d822228392aac50977
Jordan Ma <jordanma13@gmail.com>
2017-07-15 12:06:40 -0700

    page header font update

--- src/components/Header.js
+++ src/components/Header.js
@@ -83,7 +83,7 @@ class Header extends React.Component {
         <div className="header-wrapper w-max">
           <Link to="/" className="logo-module flx flx-row flx-just-start flx-align-center">
             <div className="logo-graphic">  
-              <img className="center-img" src="/img/logos/logo.bird3.lg.png"/>
+              <img className="center-img" src="/img/logos/logo.bird4.lg.png"/>
             </div>
               <div className="logo-main">
                 VIEWS
