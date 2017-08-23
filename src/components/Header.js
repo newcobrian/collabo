841bf3f977a88e6946da0953a12ac71ea80395c9
Jordan Ma <jordanma13@gmail.com>
2017-08-23 09:37:11 -0700

    Footer icons, states, and bug fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,29 +42,38 @@ const LoggedInView = props => {
         
 
         <Link to="/" onlyActiveOnIndex activeClassName="active" className="nav-module nav-feed flx flx-center-all">
+          <div className="nav-text flx flx-row flx-align-center">
+            <i className="material-icons mrgn-right-sm color--black md-24 mobile-show">language</i>
             <div className="nav-text">Explore</div>
+          </div>
         </Link>
 
         <Link to="/explore" activeClassName="active" className="nav-module nav-feed flx flx-center-all">
+          <div className="nav-text flx flx-row flx-align-center">
+            <i className="material-icons mrgn-right-sm color--black md-24 mobile-show">people</i>
             <div className="nav-text">Travelers</div>
+          </div>
         </Link>
 
         <Link to="/create" activeClassName="active" className="nav-module create nav-editor flx flx-center-all">  
           <div className="nav-text flx flx-row flx-align-center">
-            <i className="material-icons mrgn-right-sm color--primary md-24">add</i>
+            <i className="material-icons mrgn-right-sm color--success md-24 opa-100">add</i>
             <div>New Guide</div>
           </div>
         </Link>
 
         <Link to="/inbox" activeClassName="active" className="nav-module nav-notifs flx flx-center-all">
-          <div className="nav-text">
-            <InboxCounter unreadMessages={props.unreadMessages} />
+          <div className="nav-text flx flx-row flx-align-center">
+            <InboxCounter className="mrgn-right-sm" unreadMessages={props.unreadMessages} />
+            <div className="mobile-show">Activity</div>
           </div>
         </Link>
 
         <Link to={`/${props.userInfo.username}`} activeClassName="active" className="nav-module nav-profile flx flx-row flx-center-all">
-          <div className="nav-text mrgn-right-md mobile-hide">My Views</div>
-          <div className="nav-icon mrgn-none"><img className="center-img" src={props.userInfo.image}/></div>
+         <div className="nav-text flx flx-row flx-align-center">
+          <div className="nav-icon"><img className="center-img" src={props.userInfo.image}/></div>
+          <div className="mobile-show">My Views</div>
+        </div>
         </Link>
 
 
@@ -80,25 +89,25 @@ const LoggedInView = props => {
 class Header extends React.Component {
   render() {
     return (
-      <div className="header-container">
-        <div className="header-wrapper w-100">
-          <Link to="/" className="logo-module flx flx-row flx-just-start flx-align-center">
-            <div className="logo-graphic">  
-              <img className="center-img" src="/img/logos/logo.horizon.png"/>
-            </div>
-              <div className="logo-main DN">
-                VIEWS
+        <div className="header-container">
+          <div className="header-wrapper w-100">
+            <Link to="/" className="logo-module flx flx-row flx-just-start flx-align-center">
+              <div className="logo-graphic">  
+                <img className="center-img" src="/img/logos/logo.horizon.png"/>
               </div>
-              <div className="DN v2-type-caption opa-60 w-100 mrgn-top-xs mrgn-left-xs">
-                Alpha 1.2
-              </div>
-          </Link>
+                <div className="logo-main DN">
+                  VIEWS
+                </div>
+                <div className="DN v2-type-caption opa-60 w-100 mrgn-top-xs mrgn-left-xs">
+                  Alpha 1.2
+                </div>
+            </Link>
 
-          
-        </div>
-        <LoggedOutView currentUser={this.props.currentUser} />
+            
+          </div>
+          <LoggedOutView currentUser={this.props.currentUser} />
 
-        <LoggedInView currentUser={this.props.currentUser} userInfo={this.props.userInfo} unreadMessages={this.props.unreadMessages} />
+          <LoggedInView currentUser={this.props.currentUser} userInfo={this.props.userInfo} unreadMessages={this.props.unreadMessages} />
       </div>
     );
   }
