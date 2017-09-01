9ecab7f6affadb0c562a476ed1516edd27aa532c
Jordan Ma <jordanma13@gmail.com>
2017-09-01 15:34:54 -0700

    Centering Map

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -70,7 +70,8 @@ class MapContainer extends React.Component {
 	    return (	
 			<Map google={this.props.google}
 				initialCenter={initialMapCenter}
-				style={{width: '100%', height: '100%'}} >
+				style={{width: '100%', height: '100%'}}
+				className="map-wrapper" >
 		        {
 		          this.props.itinerary.tips.map((tipItem, index) => {
 		            return (
