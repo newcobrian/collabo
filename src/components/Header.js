44c01169c9d7e93bdbfbd8c151fda6e6401ca6f1
brian <leung.b@gmail.com>
2017-09-18 16:30:44 -0700

    set active class in header on popular page

--- src/components/Header.js
+++ src/components/Header.js
@@ -79,7 +79,7 @@ const LoggedInView = props => {
         
         
 
-        <Link to="/" onlyActiveOnIndex activeClassName="active" className="nav-module nav-feed flx flx-center-all">
+        <Link to="/" onlyActiveOnIndex activeClassName="active" className={"nav-module nav-feed flx flx-center-all " + (browserHistory.getCurrentLocation().pathname === '/popular' ? ' active' : '')}>
           <div className="nav-text flx flx-row flx-align-center">
             <i className="material-icons mrgn-right-sm color--black md-24 mobile-show">language</i>
             <div className="nav-text"><div className="mobile-hide">Travel</div> Guides</div>
