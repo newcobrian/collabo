a3a32c5bc47d4aff250b6658d3eee7efb936055c
brian <leung.b@gmail.com>
2018-08-05 17:31:12 -0700

    add thread page source for loadOrgUsers

--- src/components/AddThread.js
+++ src/components/AddThread.js
@@ -50,7 +50,7 @@ class AddThread extends React.Component {
     	if (!this.props.authenticated) {
     		this.props.askForAuth();
     	}
-    	this.props.loadOrgUsers(this.props.authenticated, this.props.params.orgname, Constants.THREAD_PAGE)
+    	this.props.loadOrgUsers(this.props.authenticated, this.props.params.orgname, Constants.ADD_THREAD_PAGE)
     	// this.props.sendMixpanelEvent(Constants.MIXPANEL_PAGE_VIEWED, { 'page name' : 'create guide'});
 	}
 
