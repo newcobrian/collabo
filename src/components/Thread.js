7f2df785874a5421e9e202ad4a4336e5a41097fa
funnywebdev <liubandevup@gmail.com>
2018-08-27 14:20:04 -0400

    Fix potential undefined issue

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -192,7 +192,7 @@ class Thread extends React.Component {
     this.renderChanges = (changes, docs, updates) => {
       if (changes && changes.length > 0) {
         return changes
-          .filter((c) => docs[c.fileId])
+          .filter((c) => docs && docs[c.fileId])
           .map((c, i) => (
             <div key={i}>
               <img src={c.file.iconLink} /> '{c.file.name}' was modified at <DisplayTimestamp timestamp={c.file.modifiedTime} /> by {c.file.lastModifyingUser ? c.file.lastModifyingUser.displayName : "Anonymous user"}
