b69e834e666b6d97bc25693c3816d28ad521beff
brian <leung.b@gmail.com>
2018-09-22 00:00:09 +0200

    project info component

---
+++ src/components/ProjectInfo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as Constants from '../constants';
+import ProfilePic from './ProfilePic'
+
+const ProjectInfo = props => {
+  return (
+    <div>
+      <h1>{props.project ? props.project.name : props.orgName}</h1>
+
+      <div>
+        {(props.projectMembers || []).length + ' Members'}
+      </div>
+
+      <div>
+        {props.project ? (props.project.isPublic ? 'Public' : 'Private') : ''}
+      </div>
+
+      <ul>
+        {
+          (props.projectMembers || []).map((member, index) => {
+            return (
+              <li key={member.userId}>
+                <ProfilePic src={member.image} className="user-img" /> {member.firstName} {member.lastName}
+              </li>
+            );
+          })
+        }
+
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectInfo; 
