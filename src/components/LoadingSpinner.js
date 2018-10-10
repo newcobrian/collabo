8b1bf915c3c679e605ae6725443ae34f2ca8920c
Jordan Ma <jordanma13@gmail.com>
2018-10-09 17:59:12 -0700

    Stupid comment border color

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const LoadingSpinner = props => {
 	return (
-		<div className="loading-module flx flx-col flx-center-all co-type-body fill--red">
+		<div className="loading-module flx flx-col flx-center-all co-type-body fill--primary">
           <div className="loader-wrapper flx flx-col flx-center-all">
-            <div className="v2-type-body2 color--white">{props.message}</div>
+            <div className="co-type-data color--white">{props.message}</div>
           </div>
         </div>
 	)
