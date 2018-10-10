f13f36ea3be39014ffc2b5e652adcc884c1b776a
Jordan Ma <jordanma13@gmail.com>
2018-10-09 22:05:40 -0700

    Error page

--- src/components/InvalidOrg.js
+++ src/components/InvalidOrg.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router'
 
 const InvalidOrg = props => {
 	return (
-		<div className="error-module flx flx-col flx-center-all ta-center v2-type-body3 color--black">
-          <div className="xiao-img-wrapper mrgn-bottom-sm">
-            <img className="center-img" src="/img/xiaog.png"/>
+		<div className="error-module flx flx-col flx-center-all ta-center color--white fill--primary">
+          <div className="xiao-img-wrapper mrgn-bottom-lg opa-40">
+            <img className="center-img" src="/img/logomark.png"/>
           </div>
-          <div className="mrgn-bottom-md">You don't have permission to view this org</div>
-          <Link to={'/'}>Go Home</Link>
+          <div className="mrgn-bottom-md co-type-body">You don't have permission to access this team. You must find another pond to look at.</div>
+          <Link className="color--tertiary co-type-body" to={'/'}>Return to homepage</Link>
 
         </div>
 	)
