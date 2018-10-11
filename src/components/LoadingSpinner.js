ce4f7c1cd12a131b68ad22fba49f902324f46ee6
Jordan Ma <jordanma13@gmail.com>
2018-10-10 21:41:55 -0700

    Loader graphic update

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -3,9 +3,9 @@ import React from 'react';
 const LoadingSpinner = props => {
 	return (
 		<div className="loading-module flx flx-col flx-center-all co-type-body fill--primary">
-			<div className="co-logotype w-100 mrgn-top-lg">
-			  <img className="center-img" src="/img/logotype.png"/>
-			</div>
+			<div className="loading-koi mrgn-bottom-md">
+	          <img className="center-img" src="/img/logomark.png"/>
+	        </div>
 	          <div className="loader-wrapper flx flx-col flx-center-all">
 	            <div className="co-type-data color--white">{props.message}</div>
 	          </div>
