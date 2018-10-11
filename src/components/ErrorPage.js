96eb077b57dfeed64e97ef119e8fd2978f6839c1
Jordan Ma <jordanma13@gmail.com>
2018-10-10 21:57:41 -0700

    Errorpage style

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -3,13 +3,13 @@ import { Link, browserHistory } from 'react-router'
 
 const ErrorPage = props => {
 	return (
-        <div className="home-page page-common flx flx-col flx-align-center flx-just-start ta-center">
+        <div className="error-page flx flx-col flx-center-all ta-center">
           <div className="co-logo large-logo mrgn-bottom-lg mrgn-top-md">
             <img className="center-img" src="/img/logomark.png"/>
           </div>
           <div className="mrgn-bottom-md color--white co-type-body">{ props.message ? props.message : 'Sorry, we don\'t know what happened'}
           </div>
-          <Link className="co-type-body color--tertiary" onClick={() => browserHistory.goBack()}>Go back</Link> or <Link className="co-type-body color--tertiary" to='/'>Go to the homepage</Link>
+          <Link className="co-type-body color--tertiary" onClick={() => browserHistory.goBack()}>Go back</Link> <span className="co-type-body color--white">or</span> <Link className="co-type-body color--tertiary" to='/'>Go to the homepage</Link>
         </div>
 	)
 }
