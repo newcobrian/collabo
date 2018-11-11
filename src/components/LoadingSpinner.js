114856115e7e36a99c84c8ba6d4810ea00a45395
Jordan Ma <jordanma13@gmail.com>
2018-11-11 15:06:19 -0800

    Updated global loading spinner design

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 const LoadingSpinner = props => {
 	return (
-		<div className="loading-module flx flx-col flx-center-all co-type-body fill--primary">
+		<div className="loading-module w-100 flx flx-col flx-center-all ta-center h-100 fill--mist color--black">
 			<div className="loading-koi mrgn-bottom-md">
-	          <img className="center-img" src="/img/logomark.png"/>
-	        </div>
-	          <div className="loader-wrapper flx flx-col flx-center-all">
-	            <div className="co-type-data color--white">{props.message}</div>
-	          </div>
+			  <img className="center-img" src="/img/loading-graphic.png"/>
+			</div>
+			<div className="w-100 ta-center co-type-body">Loading...</div>
         </div>
 	)
 }
