7dda278e324f000efad9502fa11bcf313e75c310
Jordan Ma <jordanma13@gmail.com>
2018-11-16 16:13:17 -0800

    No org error page

--- src/components/InvalidOrg.js
+++ src/components/InvalidOrg.js
@@ -3,14 +3,18 @@ import { Link } from 'react-router'
 
 const InvalidOrg = props => {
 	return (
-		<div className="error-module flx flx-col flx-center-all ta-center color--white fill--primary">
-          <div className="xiao-img-wrapper mrgn-bottom-lg opa-40">
-            <img className="center-img" src="/img/logomark.png"/>
-          </div>
-          <div className="mrgn-bottom-md co-type-body">You don't have permission to access this team. You must find another pond to swim in.</div>
-          <Link className="color--tertiary co-type-body" to={'/'}>Return to homepage</Link>
+		<div className="home-page page-common page-register fill--pond flx flx-col flx-just-start">
+		  <div className="koi-view ta-left flx flx-col pdding-top-lg">
+		      <div className="register-msg flx flx-col">
+		        <div className="koi-big-header mrgn-bottom-md color--utsuri opa-30">Oops...</div>
+		        <div className="co-post-title mrgn-bottom-md color--black">
+		          The team you're looking for doesn't actually exist...
+		        </div>
+		        <Link className="text-hover color--seaweed mrgn-top-sm" to='/'>Go back to the homepage</Link>
+		      </div>
+		    </div>
+		</div>
 
-        </div>
 	)
 }
 
